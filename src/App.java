3fe4664b2c5ad9d5f8ddd870550b1e51189e83c8
sandip <iamsandipsaha2005@gmail.com>
2026-05-07 11:05:52 +0530

    added initial setup

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    static void main(String[] args) {
+        System.out.println("hey");
+    }
+}
---
+++ test/AppTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void main() {
+        Mockito.mock(AppTest.class);
+        assertEquals(1,1);
+    }
+}
